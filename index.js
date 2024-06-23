34e94741caa42419a2081868cd6781e270aa7ae0
tuheen66 <hassan.monirul@gmail.com>
2024-06-23 17:33:56 +0600

    created pst api for arts and craft.

--- index.js
+++ index.js
@@ -28,7 +28,20 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
+
+    const artsCraftCollection = client
+      .db("artsCraftDB")
+      .collection("arts-craft");
+      
+
+app.post('/atrs-craft'), async(req, res)=>{
+    const artsCraft = req.body
+    const result = await artsCraftCollection.insertOne(artsCraft)
+    res.send(result)
+}
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -36,7 +49,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
