8d888c1dbbe83d7b0c5d7d72eeb69f5580478491
tuheen66 <hassan.monirul@gmail.com>
2024-06-26 04:26:43 +0600

    deployed backend into vercel

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ async function run() {
     app.delete('/art-craft/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)}
-      const result = await artsCraftCollection.deleteOne()
+      const result = await artsCraftCollection.deleteOne(query)
       res.send(result)
     })
 
@@ -105,7 +105,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
